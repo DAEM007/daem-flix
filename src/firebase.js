022f41fd6e9a57a1f491b5f11a118038e6a9e938
DAMILOLA <dammylola299@gmail.com>
2023-10-17 10:32:20 +0100

    updates to firebase config..

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+
 const firebaseConfig = {
     apiKey: "AIzaSyD47DSs4j3npUay5Drtr2pMT-VrjjEgHac",
     authDomain: "netflix-clone-9222d.firebaseapp.com",
@@ -6,3 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "482266770370",
     appId: "1:482266770370:web:2ce1b79db153f52b51c45b"
 };
+
+// initialize firebase app...
+const firebaseApp = initializeApp(firebaseConfig);
+
+// initialize database service in firestore...
+const db = getFirestore(firebaseApp);
