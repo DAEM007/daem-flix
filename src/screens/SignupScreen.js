9cff4a068e95fcb3e51b4dd6e4947f65714710a6
DAMILOLA <dammylola299@gmail.com>
2023-10-17 18:10:41 +0100

    updated component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,50 @@
 import React from 'react';
+import { useRef } from 'react';
 import "./SignupScreen.css";
+import { auth } from '../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 function SignupScreen() {
 
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
   const register = (e) => {
     e.preventDefault();
+
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => alert(err.message));
+
   };
 
   const signIn  = (e) => {
     e.preventDefault();
+
+    signInWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => alert(err.message));
+
   };
 
   return (
     <div className='signupScreen'>
         <form>
             <h1>Sign In</h1>
-            <input type='email' placeholder='Email'/>
-            <input type='password' placeholder='Password'/>
+            <input ref={emailRef} type='email' placeholder='Email'/>
+            <input ref={passwordRef} type='password' placeholder='Password'/>
             <button type='submit' onClick={signIn}>Sign In</button>
             
             <h4>
