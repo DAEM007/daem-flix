882b0a713fb0170c5c1193a86d097479b55af858
DAMILOLA <dammylola299@gmail.com>
2023-10-21 07:52:36 +0100

    updated component

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './ProfileScreen.css';
+import Nav from '../Nav';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 
 function ProfileScreen() {
-  return (
-    <div className='profileScreen'>
-        <h1>This is the profile</h1>
-    </div>
-  )
+    const user = useSelector(selectUser);
+
+    return (
+        <div className='profileScreen'>
+            <Nav />
+            <div className='profileScreen__body'>
+                <h1>Edit Profile</h1>
+                <div className='profileScreen__info'>
+                    <img 
+                        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117'
+                        alt='avatar'
+                    />
+                    <div className='profileScreen__details'>
+                        <h2>{ user.email }</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ProfileScreen;
