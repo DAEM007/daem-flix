381d009938702cd3b8a76ec3bcd26ed7bd092b01
DAMILOLA <dammylola299@gmail.com>
2023-10-21 07:53:20 +0100

    updated component

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './Nav.css';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
   const [show, handleShow] = useState(false);
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const transitionNavBar = () => {
     if(window.scrollY > 100) {
@@ -23,13 +23,14 @@ function Nav() {
     <div className={`nav ${show && `nav__black`}`}>
       <div className="nav__contents">
         <img 
+          onClick={() => navigate("/")}
           className='nav__logo'
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="netflix-logo" 
         />
 
         <img 
-          onClick={() => history.push("/profile")}
+          onClick={() => navigate("/profile")}
           className='nav__avatar'
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
           alt="avatar" 
