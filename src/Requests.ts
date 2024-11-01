82fa02142b0fc1cd3feacc604d3c8317a3bc13db
DAMILOLA <dammylola299@gmail.com>
2024-11-01 17:06:16 +0100

    ts-migrate/Requests

---
+++ src/Requests.ts
@@ -0,0 +1,15 @@
+// we would store in {process.env.API_KEY}
+const API_KEY = "4e9fca429e4ad82c14ea0eb808059545";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+};
+
+export default requests;
